179d84f1c73aba4c1b97f52b8016ac9eee2e2853
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2008-04-23 11:03:31 +0200

    add a label version
    darcs-hash:20080423090331-2fc9d-bc4375a2c6971a80204d9bf3cd87bb999ab26be0.gz

---
+++ label.cxx
@@ -0,0 +1,47 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkLabelBorderImageFilter.h"
+
+
+int main(int argc, char * argv[])
+{
+
+  if( argc != 5 )
+    {
+    std::cerr << "usage: " << argv[0] << " intput output fullyConnected bg" << std::endl;
+    std::cerr << " input: the input image" << std::endl;
+    std::cerr << " output: the output image" << std::endl;
+    std::cerr << " fullyConnected: 0 or 1" << std::endl;
+    // std::cerr << "  : " << std::endl;
+    exit(1);
+    }
+
+  itk::MultiThreader::SetGlobalMaximumNumberOfThreads(1);
+  const int dim = 3;
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::LabelBorderImageFilter< IType, IType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->SetFullyConnected( atoi(argv[3]) );
+  filter->SetBackgroundValue( atoi(argv[4]) );
+
+  itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return 0;
+}
+
